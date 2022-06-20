1bedadee2f891f374ae3a859e40dea945af5c443
Kağan Şenkeser <98644514+kagansenkeser@users.noreply.github.com>
2022-06-20 23:24:04 +0300

    Create pencere.py

---
+++ ui/pencere oluşturma/pencere.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+form =tk.Tk()
+#pencere açıyor
+form.title("pencere")
+#pencere baslihi
+yazi=tk.Label(text="deneme")
+#pencere içine yazi
+yazi.pack()
+#yaziyi kaydetme
+form.mainloop()
+#pencereyi kaydetme
+
