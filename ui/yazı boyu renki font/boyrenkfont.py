84da3aa2ea3ab570bb5bbd5025e05e3b3660f7ce
Kağan Şenkeser <98644514+kagansenkeser@users.noreply.github.com>
2022-06-20 23:29:29 +0300

    Create boyrenkfont.py

---
+++ ui/yazı boyu renki font/boyrenkfont.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+form =tk.Tk()
+form.title("pencere")
+yazi=tk.Label(text="deneme",fg="red")
+yazi.pack()
+#front
+yazi=tk.Label(text="deneme",bg="red")
+yazi.pack()
+
+yazi=tk.Label(text="deneme",font="Times 35 italic")
+yazi.pack()
+#bag
+form.mainloop()
